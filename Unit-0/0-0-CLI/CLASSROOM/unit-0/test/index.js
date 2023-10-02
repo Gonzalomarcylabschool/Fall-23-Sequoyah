afdf61b1b8387b0932b21687d2036a7bb00909ce
gonzalorportfolio <gromeroeducation@gmail.com>
2023-10-02 12:25:25 -0400

    updated with the live coding from lecture

---
+++ Unit-0/0-0-CLI/CLASSROOM/unit-0/test/index.js
@@ -0,0 +1,15 @@
+// console.log('Hello World');
+let num = 1;
+// const add = () => {
+//   for (let i = 0; i< 10; i++){
+//     num+=i;
+//   }
+//   return num;
+// }
+
+// console.log(add());
+
+while(true){
+  num+=num;
+  console.log(num);
+}//run forever
