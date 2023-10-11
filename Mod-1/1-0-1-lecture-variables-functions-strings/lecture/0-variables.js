fb2ca9a86877136a170b46ab15a947fbf06bfd0c
gonzalorportfolio <gromeroeducation@gmail.com>
2023-10-11 12:27:22 -0400

    added code from the lecture

--- Mod-1/1-0-1-lecture-variables-functions-strings/lecture/0-variables.js
+++ Mod-1/1-0-1-lecture-variables-functions-strings/lecture/0-variables.js
@@ -2,11 +2,23 @@
 const myName = 'John';
 // myName = 'Johnny'; // ERROR
 
-let myAge = 101;
+let myAge;
+// console.log(status);
+// console.log(myAge);
 myAge = 102; // OK
-
+// console.log(myAge);
 // No vars
 var status = 'happy';
-
+// console.log(status);
 // Accidentally global, also don't do
 globalStatus = 'sad';
+console.log(add(1, 2));
+
+function add (x, y) {
+  return x + y;
+}
+
+const name = 'gonzalo';
+
+console.log(name.includes('!'));
+console.log(name.indexOf('a'));
