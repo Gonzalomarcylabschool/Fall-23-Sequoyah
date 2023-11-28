40cec5c13488620a5bcd6432dbbec8cb2df5429b
gonzalorportfolio <gromeroeducation@gmail.com>
2023-11-28 11:31:03 -0500

    lecture update DOM

---
+++ Mod-2/week-2/2-2-0-intro-to-DOM/lecture/index.js
@@ -0,0 +1,9 @@
+// console.log('something')
+// console.log(document.getElementById('main-heading'));
+// console.log(document.querySelector('h3'));
+const mainHeading = document.querySelector('#main-heading')
+
+console.log(mainHeading.id);
+console.log(mainHeading.textContent);
+console.log(mainHeading.innerHTML);
+// console.log(document.querySelectorAll('li'));
