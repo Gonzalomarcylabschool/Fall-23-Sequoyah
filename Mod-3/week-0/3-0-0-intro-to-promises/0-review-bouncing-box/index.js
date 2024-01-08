f4a6ebfdb8c39dd9dbd52cd833fe38dcd9171d0f
gonzalorportfolio <gromeroeducation@gmail.com>
2024-01-08 12:39:29 -0500

    update from lecture and lab review

--- Mod-3/week-0/3-0-0-intro-to-promises/0-review-bouncing-box/index.js
+++ Mod-3/week-0/3-0-0-intro-to-promises/0-review-bouncing-box/index.js
@@ -1,25 +1,36 @@
+/* 
+1. Grab the div using dom
+2. make variables for the x axis, direction, speed, and count
+3. write a function that helps to move the div across the screen
+  * write the logic to move the div
+  * move the div using DOM
+  * change the direction
+4. invoke the main function
+*/
 const box = document.querySelector('#box');
+
 let x = 0;
-let direction = 1;
+let direction = 1; 
 let speed = 5;
+let count = 0;
 
 const update = () => {
-  // move the box
   x += speed * direction;
   box.style.left = `${x}px`;
-
-  // check for hitting the walls
-  if (x > document.body.clientWidth) {
+  if(x > document.body.clientWidth){
     direction = -1;
+    
   }
-  if (x < 0) {
+  if(x < 0 ){
     direction = 1;
+    count++;
+    box.textContent=count;
   }
 }
 
 const main = () => {
-  setInterval(update, 50)
-  console.log('starting');
-}
+  setInterval(update, 10);
+  console.log('start')
+};
 
 main();
