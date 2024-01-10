4198faab5cd5f4f647cf48d8a35cabed94d486b0
gonzalorportfolio <gromeroeducation@gmail.com>
2024-01-10 12:18:55 -0500

    updated notes from lecture

--- Mod-3/week-0/3-0-2-async-await/index.js
+++ Mod-3/week-0/3-0-2-async-await/index.js
@@ -1,8 +1,13 @@
 const body = document.body;
+// const arr = [1, 2, 3, 4]
+// arr.forEach((each) => console.log(each))
+
 
 // fetch('https://reqres.in/api/users')
 // // .then(res => console.log(res))
-//   .then(response => response.json()) // this get the response from the fetch request and changes it to JSON
+//   .then(response => {
+//     return response.json();
+//   }) // this get the response from the fetch request and changes it to JSON
 //   .then(data => console.log(data))// console.log the .json 
 //   .catch((error) => console.error(error.message));
 
@@ -32,3 +37,42 @@ const body = document.body;
 //     return res.json ( )
 //   })
 //   .then (data => console. log (data))
+
+const option = {
+    method: 'POST',
+    headers: {
+    'Content-Type': 'application/json'
+    }, 
+    body: JSON.stringify({
+      name: 'User 1'
+    })
+  }
+const fetchData = async (url, options = {}) => {
+  try {
+    const response = await fetch(url, options);
+
+    if (!response.ok) {
+      // If the response status is not ok (e.g., 404 or 500), throw an error
+      throw new Error(`Error: ${response.status} - ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    // Handle errors here
+    console.error('Error fetching data:', error.message);
+    throw error; // Re-throw the error to propagate it further if needed
+  }
+};
+
+
+const dataJson = await fetchData('https://reqres.in/api/users')
+
+console.log(dataJson.data[1]);
+
+body.innerHTML =`
+<h2>User id: ${dataJson.data[1].id}</h2>
+<p>Name: ${dataJson.data[1].first_name}</p>
+<p>Email:${dataJson.data[1].email}</p>
+
+` 
--- Mod-3/week-0/3-0-2-async-await/main.js
+++ Mod-3/week-0/3-0-2-async-await/main.js
@@ -1,1 +1,9 @@
-console.log('working')
+// console.log('working')
+
+
+// const myFetchPromise = fetch('https://reqres.in/api/users');
+// console.log(myFetchPromise);
+
+// myFetchPromise
+//   .then((response) => console.log(response));
+//   // .then((data) => console.log(data));
